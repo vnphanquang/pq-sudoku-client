a5e292a24dcb3253bd6529f46694dd647747be57
vnphanquang <linkfarius.pnq@gmail.com>
2019-09-05 12:48:01 -0700

    displayed strings in ENG

---
+++ src/lang.js
@@ -0,0 +1,12 @@
+export const DRAWER_LABELS = {
+  NEW: 'New...',
+  // SAVE: 'Save...',
+  LOAD: 'Load...',
+  EXPORT: 'Export...',
+  THEME: 'Theme',
+  SETTINGS: 'Settings',
+  FEEDBACK: 'Feedback',
+  HELP: 'Help',
+  ABOUT: 'About',
+  SOURCE: 'Source'
+}
